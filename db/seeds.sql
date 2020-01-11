c6efa055592551c8cf3954ead5cb00acee27d9cb
Sofka <sofka.vs@gmail.com>
2020-01-11 02:27:35 -0600

    done

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Turkey Burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Veggie burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Cheeseburger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Beer burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Chicken burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Special burger');
